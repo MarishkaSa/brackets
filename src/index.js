7cde5f422924ea4aaebed2e1646e573d77577c2a
MarishkaSa <borninsea@gmail.com>
2019-03-24 14:32:14 +0300

    feat: answer task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length <= 1)
+  return false
+
+let matchingOpeningBracket, ch
+let stack = []
+
+let openingBrackets = bracketsConfig.map(pair => pair[0]);
+let closingBrackets = bracketsConfig.map(pair => pair[1]);
+
+for (let i = 0; i < str.length; i++) {
+  ch = str[i]
+
+  if (closingBrackets.indexOf(ch) > -1) {
+    matchingOpeningBracket = openingBrackets[closingBrackets.indexOf(ch)]
+    if (stack.length == 0 || (stack.pop() != matchingOpeningBracket)) {
+      return false
+    }
+  } else {
+    stack.push(ch)
+  }
+}
+
+return (stack.length == 0)
 }
